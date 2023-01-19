6168536f8ddc903569581cd448a0adf2d3e48b20
vonojastro <97124440+vonojastro@users.noreply.github.com>
2023-01-19 23:52:06 +0800

    Footer Development

---
+++ components/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <section className='drop-shadow-[0_2px_5px_rgba(0,0,0,0.1)] bg-white py-5'>
+
+            <div className=' flex justify-center mx-4'>
+
+                <div>
+                    Von Ojastro
+                </div>
+
+               
+
+            </div>
+
+            <h1 className='text-center py-5'>© All Rights Reserved .Built and designed by Von Ojastro</h1>
+        </section>
+    )
+}
+
+export default Footer
