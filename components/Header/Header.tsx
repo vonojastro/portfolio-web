d764bd1795741487075c3254ea21539db01f8669
vonojastro <97124440+vonojastro@users.noreply.github.com>
2023-01-19 22:34:35 +0800

    Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai';
 import { CgClose } from 'react-icons/cg';
+import Sidebar from './Sidebar/Sidebar';
 
 
 
@@ -11,48 +12,34 @@ const Header = () => {
     return (
         <>
 
-            <div className='fixed top-0 bg-white w-full  drop-shadow '>
-                <div className='h-[70px] mx-4 flex items-center justify-between relative z-50 lg:mx-10'>
+            <div className='fixed top-0 bg-white w-full z-50 drop-shadow '>
+                <div className='h-[70px] mx-4 flex items-center justify-between relative lg:mx-10'>
 
                     <Link href='/home'>
                         <p>Von Ojastro</p>
                     </Link>
 
                     {/* NavLink large */}
-                    <ul className='space-x-5 hidden lg:flex'>
+                    <ul className='space-x-8 hidden md:flex'>
                         <li>Projects</li>
                         <li>About</li>
                         <li>Contact</li>
                         <li>Resume</li>
                     </ul>
 
-                    {/* NavLink Mobile */}
-                  
-                            <AiOutlineMenu
-                                className={`cursor-pointer relative text-2xl ${navOpen ? 'rotate-0 opacity-0' : 'rotate-180'} duration-300 ease-in-out lg:hidden`}
-                                onClick={() => setNavOpen(!navOpen)} />
-
-                 
-                                <CgClose
-                                    className={`cursor-pointer absolute right-0 text-2xl  ${!navOpen ? 'rotate-180 opacity-0' : 'rotate-0'} duration-300 ease-in-out lg:hidden`}
-                                    onClick={() => setNavOpen(!navOpen)} />
+                    <AiOutlineMenu
+                        className={`cursor-pointer relative text-2xl ${navOpen ? 'rotate-0 opacity-0' : 'rotate-180'} duration-300 ease-in-out md:hidden`}
+                        onClick={() => setNavOpen(!navOpen)} />
 
 
+                    <CgClose
+                        className={`cursor-pointer absolute right-0 text-2xl  ${!navOpen ? 'rotate-180 opacity-0' : 'rotate-0'} duration-300 ease-in-out md:hidden`}
+                        onClick={() => setNavOpen(!navOpen)} />
                 </div>
-
-
-
             </div>
 
-     
-
-            <ul className={`absolute h-full opacity-100 z-[-5] w-full flex flex-col justify-center items-center top-[0] ${navOpen ? 'right-[0px]' : 'right-[-1000px]'} duration-300 ease-in-out space-y-12 py-10 bg-white md:w-5/12 lg:hidden`}>
-                <li>Projects</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Resume</li>
-            </ul>
-
+            {/* NavLink Mobile */}
+            <Sidebar navOpen={navOpen} />
 
         </>
     )
--- components/Header/Sidebar/Sidebar.tsx
+++ components/Header/Sidebar/Sidebar.tsx
@@ -1,9 +1,36 @@
+import Link from 'next/link'
 import React from 'react'
 
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+
+type SidebarProps = {
+    navOpen: React.ReactNode
+}
+
+const Sidebar = ({ navOpen }: SidebarProps) => {
+    return (
+        <ul className={`absolute h-full opacity-100  z-0 w-full flex flex-col justify-center items-center top-[0] ${navOpen ? 'right-[0px]' : 'right-[-1000px]'} duration-300 ease-in-out space-y-7 py-10 bg-white md:hidden`}>
+            <li className='sidebarLink'>
+                <Link href='/'>
+                    Projects
+                </Link>
+            </li>
+            <li className='sidebarLink'>
+                <Link href='/'>
+                  About
+                </Link>
+            </li>
+            <li className='sidebarLink'>
+                <Link href='/'>
+                    Contact
+                </Link>
+            </li>
+            <li className='sidebarLink'>
+                <Link href='/'>
+                   Resume
+                </Link>
+            </li>
+        </ul>
+    )
 }
 
 export default Sidebar
