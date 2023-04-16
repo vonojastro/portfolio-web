f8c3342ec866e046c39a534d71772c71998cacf6
Von Ojastro <>
2023-04-16 14:00:10 +0400

    contact changes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -54,10 +54,10 @@ const Contact = () => {
 
             <form className='flex flex-col gap-4 md:gap-5 items-start w-full' onClick={onSubmit}>
               <div className='flex flex-col md:flex-row gap-4 w-full'>
-                <input type='text' placeholder='Name' className='w-full px-5 py-2 md:py-1 rounded-sm' onChange={(e) => setContactInput({ ...contactInput, name: e.target.value })} />
-                <input type='email' placeholder='Email' className='w-full px-5 py-2 md:py-1 rounded-sm' onChange={(e) => setContactInput({ ...contactInput, email: e.target.value })} />
+                <input type='text' placeholder='Name' className='w-full px-5 py-1 md:py-2 rounded-sm' onChange={(e) => setContactInput({ ...contactInput, name: e.target.value })} />
+                <input type='email' placeholder='Email' className='w-full px-5 py-1 md:py-2 rounded-sm' onChange={(e) => setContactInput({ ...contactInput, email: e.target.value })} />
               </div>
-              <textarea className='w-full px-5 py-2 md:py-1 rounded-sm h-full min-h-[150px]' onChange={(e) => setContactInput({ ...contactInput, message: e.target.value })} />
+              <textarea className='w-full px-5 py-1 md:py-2 rounded-sm h-full min-h-[150px]' onChange={(e) => setContactInput({ ...contactInput, message: e.target.value })} />
               <button className='border-[#02c272] bg-[#02c272] text-white hover:text-[#02c272] hover:bg-white px-10 py-3 duration-300 ease-in-out rounded-md'>Send Message</button>
             </form>
 
