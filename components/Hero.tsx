14798f3a67bd6e667c3ba15dc816259147cdbc6b
Von Ojastro <>
2023-04-16 13:53:16 +0400

    hero changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,15 +5,15 @@ import MyParticles from '../utils/ParticlesBG'
 const Hero = () => {
     return (
         <div className='max-h-screen mx-auto w-full lg:max-w-6xl'>
-            <div className='flex justify-between gap-10 px-5 items-center w-full text-center min-h-screen  md:px-8'>
+            <div className='flex justify-start gap-10 px-5 items-center w-full text-center min-h-screen  md:px-8'>
                 <MyParticles
                     id='tsparticle'
                     // particlesLoaded='particlesLoaded'
                     options={particlesConfig}
                 />
- 
 
-                <div className='flex w-full  flex-col space-y-2 md:space-y-6 justify-center px-4 lg:px-0'>
+                
+                <div className='flex w-8/12 flex-col space-y-2 md:space-y-6 justify-center px-4 lg:px-0'>
                     <div className='flex flex-col gap-1 text-center sm:text-start '>
                         <h3 className='text-xl text-white'>Hello, <span className='text-[#02c272]'>I'm</span></h3>
                         <h1 className='font-bold text-2xl lg:text-4xl text-[#02c272]' >Von Carlo R. Ojastro</h1>
@@ -24,6 +24,8 @@ const Hero = () => {
                         <button className='w-[200px] px-10 py-3 mt-6 bg-[#02c272] text-white rounded-lg hover:bg-white hover:text-[#02c272] drop-shadow-[0_35px_35px_rgba(255,255,255,0.15)] duration-200'>Visit Page</button>
                     </div>
                 </div>
+
+     
             </div>
         </div>
     )
